e5cc5d30cf58619594006342593769a056ad2524
O-Alexsandro <alexsandrooliveiraribas@gmail.com>
2025-06-19 21:00:57 -0300

    primeiro commit

---
+++ src/main/java/com/projeto_aws/Inventarioti/InventarioTiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_aws.Inventarioti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioTiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto_aws/Inventarioti/InventarioTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_aws.Inventarioti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
